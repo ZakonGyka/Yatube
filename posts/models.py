a98d619c6e16be55197935638afa6ea54012d9a9
Петр Лукахин <zakongyka@yandex.ru>
2020-12-02 18:41:20 +0300

    fix CustomUser|Needed chapter

--- posts/models.py
+++ posts/models.py
@@ -15,29 +15,6 @@ class CustomUser(AbstractBaseUser):
         unique=True,
         verbose_name=_('email address'),
     )
-    username = models.CharField(
-        _('username'),
-        max_length=150,
-        unique=True,)
-
-    first_name = models.CharField(_('first name'), max_length=150, blank=True)
-    last_name = models.CharField(_('last name'), max_length=150, blank=True)
-
-    is_active = models.BooleanField(
-        _('active'),
-        default=True,
-        help_text=_(
-            'Designates whether this user should be treated as active. '
-            'Unselect this instead of deleting accounts.'
-        ),
-    )
-    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
-
-    objects = BaseUserManager()
-
-    EMAIL_FIELD = 'email'
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
 
     class Meta:
         verbose_name = _('user')
