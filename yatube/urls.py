70050448b92caf0d80b0157b8c8319d37ed206b7
Петр Лукахин <zakongyka@yandex.ru>
2020-12-10 02:41:39 +0300

    clean code| main urls

--- yatube/urls.py
+++ yatube/urls.py
@@ -3,15 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.flatpages import views
 from django.urls import include, path
-from posts import views as posts_views
-from django.conf.urls import handler404, handler500
 
 handler404 = "posts.views.page_not_found" # noqa
 handler500 = "posts.views.server_error" # noqa
 
 urlpatterns = [
-    # path("404/", posts_views.page_not_found, name="page_not_found"),
-    # path("500/", posts_views.server_error, name="server_error"),
     path('admin/', admin.site.urls),
     path('about/', include('django.contrib.flatpages.urls')),
     path('auth/', include('users.urls')),
